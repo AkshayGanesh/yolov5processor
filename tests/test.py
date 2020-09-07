acba34e5b53002f1c5389c7ee5d7f96e9822dba0
Akshay G <akshayganesh1997@gmail.com>
2020-09-07 10:26:02 +0530

    Bug fix

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,7 @@ from yolov5processor.infer import ExecuteInference
 
 cap = cv2.VideoCapture("cement_bag_detection.avi")
 yp = ExecuteInference(weight="last_cement.pt", gpu=False)
-yp.__init__()
+yp.__init__(weight="last_cement.pt", gpu=False)
 ret = True
 while ret:
     ret, frame = cap.read()
