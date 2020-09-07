9abef60d9fa5a853b3ff9324256d7ba190df07ab
Akshay G <akshayganesh1997@gmail.com>
2020-09-07 09:24:29 +0530

    Initial push

--- setup.py
+++ setup.py
@@ -19,4 +19,17 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='==3.7',
+    install_requires =[
+        "Cython",
+        "matplotlib>=3.2.2",
+        "numpy>=1.18.5",
+        "opencv-python>=4.1.2",
+        "pillow",
+        "PyYAML>=5.3",
+        "scipy>=1.4.1",
+        "tensorboard>=2.2",
+        "torch>=1.6.0",
+        "torchvision>=0.7.0",
+        "tqdm>=4.41.0"
+    ]
 )
