788accfa93798729c002b2c9b4f943284ff97cad
Akshay G <akshayganesh1997@gmail.com>
2020-10-07 09:26:09 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,7 @@
 import cv2
 from yolov5processor.infer import ExecuteInference
-cap = cv2.VideoCapture("/home/administrator/akshay/yolov5processor/tests/cement_bag_detection.avi")
-yp = ExecuteInference(weight="/home/administrator/akshay/yolov5processor/tests/last_cement.pt", gpu=True)
+cap = cv2.VideoCapture("/home/administrator/akshay/yolov5processor/tests/video.avi")
+yp = ExecuteInference(weight="/home/administrator/akshay/yolov5processor/tests/weight.pt", gpu=True)
 ret = True
 while ret:
     ret, frame = cap.read()
